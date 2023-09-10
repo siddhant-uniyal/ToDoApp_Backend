a424e66c4063b88ced5a3331cda7c2b31008aa77
Siddhant Uniyal <siddhantuniyal416@gmail.com>
2023-09-10 07:14:51 +0530

    Added .get in server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ import {connectToDB} from "./data/db.js"
 connectToDB();
 
 
-
+app.get("/" , (req , res)=>{
+    res.send("site is working")
+})
 app.listen(process.env.PORT , ()=>{
     console.log(`Server is working on port : ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
 })
